64d2f646b7edbbf526c0b791dba56a3b23f6c37e
nobar <nobar.hassan@gmail.com>
2023-04-24 20:55:08 +0300

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,2 @@
+// to instal npm initi type in terminal npm init -y for default instal
+// to instal npm lodash type in terminal npm i lodash
---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const names = require('./2-names')
+const sayHi = require('./3-utils')
+const data = require('./4-alternative-flavours')
+
+require('./5-mind-grenade')
+// console.log(data)
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
